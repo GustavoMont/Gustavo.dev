41e23da8e62282c15e3f09f663ca6960b0fe49d5
Luis Gustavo <l.monteiro@ctrlplay.com.br>
2022-05-17 01:15:45 -0300

    save changes

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
+import { useRouter } from "next/router";
 import React from "react";
 import getAllPosts from "../../functions/getAllPosts";
 import Post, { CategoryPosts } from "../../models/Posts";
@@ -9,12 +10,16 @@ import Card from "../../styles/Posts/Card";
 import H2 from "../../styles/Typography/H2";
 
 export default function Posts({ posts }: any) {
+  const router = useRouter();
   return (
     <Layout>
       <H2 color={"secondary-pink"}>Todos os Posts</H2>
       <CardContainer>
         {posts.map((post: Post, index: number) => (
-          <Card key={index} clickable>
+          <Card
+            key={index}
+            clickable
+            onClick={() => router.push(`posts/${index.toString()}`)}>
             <div className="image-container">
               <img src={post.metadata.banner} alt="A" />
             </div>
