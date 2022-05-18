b3458502622e281447b90f28546904f879038efa
Luis Gustavo <l.monteiro@ctrlplay.com.br>
2022-05-18 20:21:30 -0300

    edit post

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,12 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
 import { useRouter } from "next/router";
 import React from "react";
+import ReactMarkdown from "react-markdown";
 import getAllPosts from "../../functions/getAllPosts";
+import getPost from "../../functions/getPost";
 import Layout from "../../styles/Global/Layout";
+import H2 from "../../styles/Typography/H2";
 
 export default function Post(props: any) {
+  console.log(props.post);
+
   return (
     <Layout>
-      <div dangerouslySetInnerHTML={{ __html: props.post.content }} />
+      <div>
+        <img src={props.post.metadata.banner} alt="AAAAAAAAAA" />
+      </div>
+      <H2>{props.post.metadata.title}</H2>
+      <ReactMarkdown linkTarget={"_blank"}>{props.post.content}</ReactMarkdown>
     </Layout>
   );
 }
@@ -18,15 +28,14 @@ export async function getStaticPaths() {
   });
   return {
     paths,
-    fallback: true, // false or 'blocking'
+    fallback: "blocking", // false or 'blocking'
   };
 }
 
 export async function getStaticProps(context: any) {
-  const posts = getAllPosts();
-  const post = posts.find(
-    ({ metadata }) => metadata.slug === context.params.slug
-  );
+  const post = getPost(context.params.slug);
+  console.log(post);
+
   return {
     props: {
       post,
---
+++ posts/ola-mundo.md
@@ -0,0 +1,17 @@
+---
+title: Olá ,mundo
+category: talk-in-code
+abstract: Primeiro posts de vários posts fututos.
+date: "2022-05-18"
+banner: https://i.pinimg.com/600x315/b6/55/f8/b655f85c886963d5aedcc062748cfaaf.jpg
+---
+
+Bem... Não sei escrever isso, tchau!
+
+---
+
+---
+
+---
+
+Brincadeira falando sério agora.
